f008acf8688ec1f2383097c07e0371d94fc9bfe0
Marta Prystatska <tradlisboa@gmail.com>
2024-04-08 18:44:18 +0100

    Start  js

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert("ten");
+// alert(77);
+// alert("true");
+//comment one line
+
+/*
+many line
+comment
+*/
+console.log('ten');      //string
+console.log(18.45);      //number   escrito a mão-hardcode
+console.log(10n);         //bigint
+console.log(true);        //boolean
+console.log(undefined);   //undefined
+console.log(null);        //object
