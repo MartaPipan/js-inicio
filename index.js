41828e4e3db13eddc9ed0cd6a50281e6589d1991
Marta Prystatska <tradlisboa@gmail.com>
2024-04-09 09:51:59 +0100

    Grouping(); Access. ; +; %; operators js

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-console.log('ten');      //string
-console.log(18.45);      //number   escrito a mão-hardcode
-console.log(10n);         //bigint
-console.log(true);        //boolean
-console.log(undefined);   //undefined
-console.log(null);        //object
+// console.log('ten');      //string
+// console.log(18.45);      //number   escrito a mão-hardcode
+// console.log(10n);         //bigint
+// console.log(true);        //boolean
+// console.log(undefined);   //undefined
+// console.log(null);        //object
 
-let ageAnimal;
-//let age_animal;
-const amountPawsAnimal = 4;
+// let ageAnimal;
+// //let age_animal;
+// const amountPawsAnimal = 4;
 
 
-const userFirstName = 'Macha';
-console.log('userFirstName = ', userFirstName);
-const userAge = 23;
-console.log('userAge =' userAge);
-const nameProgrammingLanguage = 'C++';
-const isAdultUser = true;
-const hasUkrainianCitizenship = null;
-let userYearBirthday = undefined;
+// const userFirstName = 'Macha';
+// console.log('userFirstName = ', userFirstName);
+// const userAge = 23;
+// console.log('userAge =' userAge);
+// const nameProgrammingLanguage = 'C++';
+// const isAdultUser = true;
+// const hasUkrainianCitizenship = null;
+// let userYearBirthday = undefined;
 
-console.log('title book: "qwerty"');
-console.log("I'm a teacher");
-console.log('I\'m a teacher');
+// console.log('title book: "qwerty"');
+// console.log("I'm a teacher");
+// console.log('I\'m a teacher');
 
-const login = 'qwerty';
-{
-    const login = 'www'
-    console. log('login = ', login)
-}
-{ 
-    const login = 'eee'
-    
-}
+// const login = 'qwerty';
+// {
+//     const login = 'www'
+//     console. log('login = ', login)
+// }
+// { 
+//     const login = 'eee'
+//     console.log('eee = ', eee);
+// }
+
+// console.log(-5 % 7); //-5;
+// console.log(15 % 7); //1;
+// console.log(9 % -2); //1
+
+const numberFromDB = 4;
+const numberForUser = '5';
+// 4 + '5' =45
+const userFirstName = Bred;
+const userLastName = Pit;
+const userFullName = userFirstName + '' + userLastName;
+console.log( 'userFullName = ' userFullName);
+
+const result = numberFromDB + numberForUser;
+console.log('result =', result);
