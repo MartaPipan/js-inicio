c370c1747d82a0c9c2272d8d344d90b47c4ba300
Marta Prystatska <tradlisboa@gmail.com>
2024-04-10 09:31:56 +0100

    homework 09.04.2024

---
+++ indexhome.js
@@ -0,0 +1,16 @@
+//- температури повітря у Цельсіях (24)
+const airTemperatureCelsius = 24;
+
+// назви книжки (undefined)
+let bookTitleUndefined;
+
+//- довжини (3) та ширини (4) прямокутника; площі прямокутника
+const rectangleLength = 3;
+const rectangleWidth = 4;
+const rectangleArea = rectangleLength * rectangleWidth;
+
+//- чи є фігура трикутником (false)
+const isTriangle = false;
+
+//елемент(null)
+const element = null;
