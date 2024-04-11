1b6d358ee957cebc6d0a888bbabdebe4950a1608
Marta Prystatska <tradlisboa@gmail.com>
2024-04-11 08:05:14 +0100

    exponentiation **

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ let number2 = 2;
 let result = number + ++number2;
 // let result = number-- + ++number2;
 // let result = number + number2++;
+
+const base = 2;
+const pow = 3;
+const result = base ** pow;
+// const result = (-base) ** pow;
+//const result = -(base ** pow);
