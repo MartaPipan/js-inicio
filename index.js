f324a1c9fdecc76791e12ae7427004a4a3f3f743
Marta Prystatska <tradlisboa@gmail.com>
2024-04-11 07:53:03 +0100

    increment and decrement; prefix and postfix

--- index.js
+++ index.js
@@ -1,69 +1,12 @@
-// console.log('ten');      //string
-// console.log(18.45);      //number   escrito a mão-hardcode
-// console.log(10n);         //bigint
-// console.log(true);        //boolean
-// console.log(undefined);   //undefined
-// console.log(null);        //object
+let number1 = 10;
+console.log(++number1);
+console.log(number1); //11
 
-// let ageAnimal;
-// //let age_animal;
-// const amountPawsAnimal = 4;
+console.log(number1++);
+console.log(number1); //11
 
-
-// const userFirstName = 'Macha';
-// console.log('userFirstName = ', userFirstName);
-// const userAge = 23;
-// console.log('userAge =' userAge);
-// const nameProgrammingLanguage = 'C++';
-// const isAdultUser = true;
-// const hasUkrainianCitizenship = null;
-// let userYearBirthday = undefined;
-
-// console.log('title book: "qwerty"');
-// console.log("I'm a teacher");
-// console.log('I\'m a teacher');
-
-// const login = 'qwerty';
-// {
-//     const login = 'www'
-//     console. log('login = ', login)
-// }
-// { 
-//     const login = 'eee'
-//     console.log('eee = ', eee);
-// }
-
-// console.log(-5 % 7); //-5;
-// console.log(15 % 7); //1;
-// console.log(9 % -2); //1
-
-// const numberFromDB = 4;
-// const numberForUser = '5';
-// // 4 + '5' =45
-// const userFirstName = Bred;
-// const userLastName = Pit;
-// const userFullName = userFirstName + '' + userLastName;
-// console.log( 'userFullName = ' userFullName);
-
-const numberFromDB = 4;
-const numberForUser = '5';
-const conversionNumberForUser = Number(numberForUser);
-const result = numberFromDB + conversionNumberForUser;
-console.log('result =', result);
-console.log( 'typeof result = ', typeof result);
-
-console.log(12 % 0); //NaN
-console.log(12 + null);  //NaN
-console.log(12 * ''); //NaN
-
-//
-//NaN
-
-//0 ~0 + 0;
-// ''
-// null
-// false 
-//undefined
-//+ - * / %
-
-// 1  true
+let number = 1;
+let number2 = 2;
+let result = number + ++number2;
+// let result = number-- + ++number2;
+// let result = number + number2++;
