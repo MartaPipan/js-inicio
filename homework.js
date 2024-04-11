5cf4fa78bd6120e76b15da71e8fe5a990ac8e85f
Marta Prystatska <tradlisboa@gmail.com>
2024-04-12 00:24:08 +0100

    homework 11-04.2024

---
+++ homework.js
@@ -0,0 +1,26 @@
+let totalMyBooks= 240;
+let totalMyReadBooks= 60;
+let percentageMyReadBooks = (totalMyReadBooks / totalMyBooks) * 100;
+console.log(totalMyBooks);
+console.log(totalMyReadBooks);
+console.log(percentageMyReadBooks);
+
+let myMoneyHryvnia = 5442;
+let exchangeRate = 42.09;
+let myMoneyEuro = myMoneyHryvnia / exchangeRate;
+console.log(myMoneyHryvnia);
+console.log(exchangeRate);
+console.log(myMoneyEuro);
+
+let restaurantDishMeat = 28;
+let restaurantDishSalad = 5;
+let restaurantDrink = 12;
+let restaurantOrder = restaurantDishMeat + restaurantDishSalad + restaurantDrink;
+let restaurantGratuity = (restaurantOrder / 100) * 15;
+let totalRestaurantOrder = restaurantOrder + restaurantGratuity;
+console.log(restaurantDishMeat);
+console.log(restaurantDishSalad);
+console.log(restaurantDrink);
+console.log(restaurantOrder);
+console.log(restaurantGratuity);
+console.log(totalRestaurantOrder);
